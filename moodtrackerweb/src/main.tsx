a0ddd3c8d4df4bc908bb4f6b1a9e975b32629e91
Eurydice Soderquist <contact@dakkra.com>
2024-10-28 23:02:36 -0600

    Setup react router

--- moodtrackerweb/src/main.tsx
+++ moodtrackerweb/src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import App from "./App.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App></App>,
+    },
+    {
+        path: "/demo",
+        element: <>{"Demo!"}</>
+    }
+]);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <RouterProvider router={router}/>
+    </StrictMode>
 )
